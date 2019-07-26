2696bf14e63a9d64912011af8d4bab33d35c8efa
John Webster <JohnLobster@comcast.net>
2019-07-25 17:24:33 -0700

    Added bootstrap modal to title, needs styling and help content

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './index.css';
+import HelpModal from "../HelpModal";
 
 class Title extends Component {
   render () {
@@ -10,6 +11,8 @@ class Title extends Component {
         <h1 className="text-white pl-5 pt-2 pb-3 mb-0" >
           <span className="titleSpan pr-3">Memory Game</span>
           <img src={logo} alt="React logo" className="logo"></img>
+          {/* help button and modal */}
+          <HelpModal />
         </h1>
       </div>
     )
